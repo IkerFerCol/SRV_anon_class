c978495f4f78df2f1092fc1eb80f9600fb3d4b97
ikefercol <ikefercol@alu.edu.gva.es>
2024-09-12 11:40:59 +0200

    aaa

--- E1/src/Main.java
+++ E1/src/Main.java
@@ -1,16 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+class Persona {
+    // ...
+}
+interface Objeto {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-            System.out.println("ADIOS");
-        }
+}
+
+class Alumne extends Persona {
+    // ...
+}
+
+
+
+public class Main {
+
+    static void imprimirJerarquiaDeClasses(Class<?> c) {
+        System.out.print(c.getName() + (c.getSuperclass() != null ? "  >  " : "\n"));
+        if (c.getSuperclass() == null) return;
+        imprimirJerarquiaDeClasses(c.getSuperclass());
+    }
+
+    public static void main(String[] args) {
+
+        Alumne alumne = new Alumne(){};
+        Objeto inter = new Objeto(){};
+
+
+        imprimirJerarquiaDeClasses(alumne.getClass());
+        imprimirJerarquiaDeClasses(inter.getClass());
+
     }
 }
